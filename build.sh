1d066756a8f65441cdeb2b914e9a290e9bdd4ea0
Andrew Seif <Andrew.Seif@outlook.com>
2024-10-04 16:39:16 +0300

    fixed build.sh

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ echo $clientSecret
 
 # Run Maven build to create the JAR file
 echo "Running Maven build..."
-mvn clean package
+mvn package
 
 
 # Build the Docker image with the provided clientId and clientSecret
@@ -18,7 +18,7 @@ docker build -t sahha-app .
 echo "Docker image built successfully!"
 
 echo "Running Docker image..."
-docker run --env-file .env -d sahha-app -p 8081:8080
+docker run -p 8081:8080 --env-file .env -d sahha-app
 
 
 # Display a success message
