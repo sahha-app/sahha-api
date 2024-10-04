a39d0709ca4087658308b603e06ed88c6ad50db6
Andrew Seif <Andrew.Seif@outlook.com>
2024-10-04 04:41:36 +0300

    added biomarker controller

--- src/main/java/com/sahha/app/controller/BiomarkersController.java
+++ src/main/java/com/sahha/app/controller/BiomarkersController.java
@@ -6,10 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 @RestController
 @RequestMapping("/public/api/v1/biomarkers")
@@ -19,15 +16,47 @@ public class BiomarkersController {
             .baseUrl("https://sandbox-api.sahha.ai")
             .build();
     private static String externalId = "4B06C59F-A3AE-42AE-A178-E32A6F061B18";
-    private static String accountsToken ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwczovL2FwaS5zYWhoYS5haS9jbGFpbXMvYWNjb3VudElkIjoiOTk2NzE2ZmMtNDUzMC00MjJkLTg4OWYtOTljZDEzY2IxYjJmIiwiaHR0cHM6Ly9hcGkuc2FoaGEuYWkvY2xhaW1zL2FkbWluIjoiVHJ1ZSIsImh0dHBzOi8vYXBpLnNhaGhhLmFpL2NsYWltcy9hY2NvdW50IjoiVHJ1ZSIsImh0dHBzOi8vYXBpLnNhaGhhLmFpL2NsYWltcy9zYWhoYUFwaVNjb3BlIjoiU2FuZGJveCIsImV4cCI6MTczMDA0MTI2MywiaXNzIjoiaHR0cHM6Ly9zYWhoYS1wcm9kdWN0aW9uLmF1LmF1dGgwLmNvbS8iLCJhdWQiOiJodHRwczovL3NhaGhhLXByb2R1Y3Rpb24uYXUuYXV0aDAuY29tL2FwaS92Mi8ifQ.fC536zQCDwIyqAObFGwj6Jk5iIdPsXNPr8NPFp6qOfg";
-    @PostMapping("/get")
-    public ArrayList getBioMarkers(){
-        ArrayList biomarkers = client1.get()
-                .uri("/api/v1/profile/biomarker/"+externalId)
-                .header("Authorizations",accountsToken)
+    private static String accountsToken ="Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwczovL2FwaS5zYWhoYS5haS9jbGFpbXMvYWNjb3VudElkIjoiOTk2NzE2ZmMtNDUzMC00MjJkLTg4OWYtOTljZDEzY2IxYjJmIiwiaHR0cHM6Ly9hcGkuc2FoaGEuYWkvY2xhaW1zL2FkbWluIjoiVHJ1ZSIsImh0dHBzOi8vYXBpLnNhaGhhLmFpL2NsYWltcy9hY2NvdW50IjoiVHJ1ZSIsImh0dHBzOi8vYXBpLnNhaGhhLmFpL2NsYWltcy9zYWhoYUFwaVNjb3BlIjoiU2FuZGJveCIsImV4cCI6MTczMDA3NzUzMCwiaXNzIjoiaHR0cHM6Ly9zYWhoYS1wcm9kdWN0aW9uLmF1LmF1dGgwLmNvbS8iLCJhdWQiOiJodHRwczovL3NhaGhhLXByb2R1Y3Rpb24uYXUuYXV0aDAuY29tL2FwaS92Mi8ifQ.M6xh1HNQ91fQfdwKEeBHpwZHrSsPPEz-oq6FynzgcUI";
+    private static String profilesToken = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwczovL2FwaS5zYWhoYS5haS9jbGFpbXMvcHJvZmlsZUlkIjoiNjU2OTFkOWQtMGNhYi00MzY1LTg1MDMtYzY3MmU3YTI5ZDgwIiwiaHR0cHM6Ly9hcGkuc2FoaGEuYWkvY2xhaW1zL2V4dGVybmFsSWQiOiI0QjA2QzU5Ri1BM0FFLTQyQUUtQTE3OC1FMzJBNkYwNjFCMTgiLCJodHRwczovL2FwaS5zYWhoYS5haS9jbGFpbXMvYWNjb3VudElkIjoiOTk2NzE2ZmMtNDUzMC00MjJkLTg4OWYtOTljZDEzY2IxYjJmIiwiaHR0cHM6Ly9hcGkuc2FoaGEuYWkvY2xhaW1zL3NhaGhhQXBpU2NvcGUiOiJTYW5kYm94IiwiZXhwIjoxNzMwNTU1MjQ0LCJpc3MiOiJodHRwczovL3NhbmRib3gtYXBpLnNhaGhhLmFpIiwiYXVkIjoiaHR0cHM6Ly9zYW5kYm94LWFwaS5zYWhoYS5haSJ9.sKyoaneJyc2mzeNqx5YN2q2mhHEdAEf8nViq-vl9REc";
+    @GetMapping("/steps")
+    public ArrayList<Map<String,Object>> getBioMarkers(){
+        ArrayList<Map<String,Object>> biomarkers = client1.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/api/v1/profile/biomarker/"+externalId)
+                        .queryParam("types", List.of("steps"))
+                        .build())
+                .header("Authorization", "account " + accountsToken)
                 .retrieve()
                 .bodyToMono(ArrayList.class)
                 .block();
         return biomarkers;
+    }
+
+    @PostMapping("/profile")
+    public Map<String,Object> getProfileToken(){
+        Map<String,String> bodyValues = new HashMap<>();
+        bodyValues.put("externalId",externalId);
+        Map<String,Object> profileToken = client1.post()
+                .uri("/api/v1/oauth/profile/token")
+                .bodyValue(bodyValues)
+                .header("Authorization", "profile " + accountsToken)
+                .retrieve()
+                .bodyToMono(Map.class)
+                .block();
+        return profileToken;
+    }
+
+    @GetMapping("/profile/biomarker")
+    public Map<String,Object> getProfileBiomarker(){
+        Map<String,Object> profileBiomarker= client1.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("api/v1/profile/biomarker")
+                        .queryParam("types", "height")
+                        .build())
+                .header("Authorization", "profile " + profilesToken)
+                .retrieve()
+                .bodyToMono(Map.class)
+                .block();
+        return profileBiomarker;
     }
 }
