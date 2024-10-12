dd38c6b8c4683af21652c6cd6d86016bd10e8d38
mustafa-abuelmagd <mustafa.2buelmagd@gmail.com>
2024-10-12 16:18:59 +0300

    feat: security so far

---
+++ src/main/java/com/sahha/app/config/SecurityAdapter.java
@@ -0,0 +1,32 @@
+package com.sahha.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityAdapter {
+
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(a -> a
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .oauth2Login(Customizer.withDefaults());
+        return http.build();
+    }
+
+
+}
