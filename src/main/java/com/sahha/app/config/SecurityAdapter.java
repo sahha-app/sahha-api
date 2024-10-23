f7d985a4202e249328e554d2dc8e602fab1cfd85
mustafa-abuelmagd <mustafa.2buelmagd@gmail.com>
2024-10-23 22:49:05 +0300

    fix: configuring session management

--- src/main/java/com/sahha/app/config/SecurityAdapter.java
+++ src/main/java/com/sahha/app/config/SecurityAdapter.java
@@ -1,30 +1,36 @@
 package com.sahha.app.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 
-@Configuration
 @EnableWebSecurity
+//@EnableRedisHttpSession
 public class SecurityAdapter {
 
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(a -> a
-                        .requestMatchers("/", "/sendEmail", "/error", "/webjars/**").permitAll()
+                        .requestMatchers("/","/login",  "/error").permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(e -> e
                         .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                 )
-                .oauth2Login(Customizer.withDefaults());
+                .oauth2Login(Customizer.withDefaults())
+                .sessionManagement(Customizer.withDefaults());
+//                .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.cl)
+
+
+//                .maximumSessions(1)
+//                .maxSessionsPreventsLogin(true);
         return http.build();
     }
 
